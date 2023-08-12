b8ebd672998f752ecd087d5a8028ea8bc9502053
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2023-08-12 18:56:03 +0100

    Add PHP session_id to filenames

--- src/python/hclust_esc.py
+++ src/python/hclust_esc.py
@@ -1,26 +1,38 @@
 from collections import OrderedDict
 import sys, json
+import os.path
 import scipy
 import scipy.cluster.hierarchy as sch
 import pandas as pd
-import matplotlib
+
+
+sess_id = json.loads(sys.argv[1])
+
+sess_infile = "_inputfile.json"
+sess_outfile = "_outputfile.json"
+
+conc_in_file = sess_id + sess_infile
+conc_out_file = sess_id + sess_outfile
+
+root = '/var/www/devseqplant.org/files'
+
+file_path_input = os.path.join(root, conc_in_file)
+file_path_output = os.path.join(root, conc_out_file)
 
 
 # Load the data from json file (only if >400 genes selected)
 try:
-   with open('/Applications/XAMPP/xamppfiles/htdocs/inputfile.json', 'r') as inputfile:
+   with open(file_path_input, 'r') as inputfile:
       data = json.load(inputfile, object_pairs_hook=OrderedDict) 
 except (ValueError, TypeError, IndexError, KeyError) as e:
      print (json.dumps({'error': str(e)}))
      sys.exit(1)
 
-data2 = json.loads(json.dumps(data))
-
 
 ####################### Perform data clustering here #########################
 
 # create pandas data frame
-df = pd.DataFrame(data2)
+df = pd.DataFrame(data)
 
 # invert pandas dataframe
 dfinv=df.T
@@ -45,5 +57,5 @@ myorder = ident_reord
 ordered_data = OrderedDict((k, data[k]) for k in myorder)
 
 # write hclust reordred data to json outputfile
-with open('/Applications/XAMPP/xamppfiles/htdocs/outputfile.json', 'w') as outputfile:
+with open(file_path_output, 'w') as outputfile:
    json.dump(ordered_data, outputfile)
