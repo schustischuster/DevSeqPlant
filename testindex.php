9ab92782ca0c4529a2b93e8048f3ef28003f0ebf
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2023-10-12 05:29:04 +0100

    Update fa external links in mega menu dropdown

--- testindex.php
+++ testindex.php
@@ -701,19 +701,15 @@ HTML Body starts: wrapper = 1st level div
                 <div class="childmmf MMnum2">
                   <div class = "MMchild2lnk">
                   <font color="#0d0e0f">More information on: &nbsp&nbsp</font>
-                    <div class='hreflinks15'><a>
+                    <div class='hreflinks15'><a><span>
                       <svg class="fa_external_link" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
-                      <div class="tooltipwrapper" style="display:inline-block;">
-                        <div><a class='hreflinks13' href="#" title="Go to Data Analysis Methods">Data Analysis Methods</a></div>
-                      </div>
-                    </a></div>
+                      <a class='hreflinks13' href="#" title="Go to Data Analysis Methods">Data Analysis Methods</a>
+                    </a></span></div>
                     &nbsp &nbsp
-                    <div class='hreflinks15'><a>
+                    <div class='hreflinks15'><a><span>
                       <svg class="fa_external_link" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
-                      <div class="tooltipwrapper" style="display:inline-block;">
-                        <div><a class='hreflinks13' href="#" title="Go to Datasets">Datasets</a></div>
-                      </div>
-                    </a></div>
+                      <a class='hreflinks13' href="#" title="Go to Datasets">Datasets</a>
+                    </a></span></div>
                   </div>
                 </div>
               </div><!-- Close "Mega_Menu_LowerspacerTxt" -->
