2e07c61105f0ca439e8d02180aea223c10856359
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2023-10-22 09:23:58 +0100

    Update var names and data processing

--- src/r/Prepare_MT_transcripts_data.R
+++ src/r/Prepare_MT_transcripts_data.R
@@ -1,58 +1,19 @@
-# Prepare_ATH_data_for_webpage - expression data version from 20210528
-# This script prepares DevSeq A.thaliana expression data for DevSeq webpage database
+# Prepare_MT_data_for_webpage - expression data version from 20210528
+# This script prepares DevSeq M.truncatula expression data for DevSeq webpage database
 # Input format of DevSeq expression table is as follows:
-# id / symbol / biotype / source / DEVSEQ_SAMPLE_REPLICATES(132samples)
-# Check: depending on input file version, input format could be without symbols
-# id / biotype / source / DEVSEQ_SAMPLE_REPLICATES(132samples)
-# In that case, load gene_ids_araport_gene_symbol file and add gene symbols to
-# devseq expression table
+# id / symbol / biotype / source / DEVSEQ_SAMPLE_REPLICATES(24/27samples for table w/w/o pollen)
 
-devseq_transcripts_input_file <- "AT_transcripts_inter_norm_tpm_mat_deseq_sample_names.csv"
-devseq_transcripts_input_file_pol <- "AT_transcripts_intra_norm_tpm_mat_deseq_sample_names.csv"
-id_symbol_input_file <- "Gene_IDs_ATH_names_wo_dupl.csv"
+devseq_transcripts_input_file <- "MT_transcripts_inter_norm_tpm_mat_deseq_sample_names.csv"
+devseq_transcripts_input_file_pol <- "MT_transcripts_intra_norm_tpm_mat_deseq_sample_names.csv"
 
 
 # Read raw data
-devseq_transcripts_all_samples <- read.table(file=file.path(in_dir, "AT", devseq_transcripts_input_file), sep=";", dec=".", header=TRUE, stringsAsFactors = FALSE)
-devseq_transcripts_all_samples_pol <- read.table(file=file.path(in_dir, "AT", devseq_transcripts_input_file_pol), sep=";", dec=".", header=TRUE, stringsAsFactors = FALSE)
-gene_id_symbol_list <- read.table(file=file.path(in_dir, "AT", id_symbol_input_file), sep=";", dec=".", header=TRUE, stringsAsFactors = FALSE)
+devseq_transcripts_all_samples <- read.table(file=file.path(in_dir, "MT", devseq_transcripts_input_file), sep=";", dec=".", header=TRUE, stringsAsFactors = FALSE)
+devseq_transcripts_all_samples_pol <- read.table(file=file.path(in_dir, "MT", devseq_transcripts_input_file_pol), sep=";", dec=".", header=TRUE, stringsAsFactors = FALSE)
 
 
 devseq_transcripts_all_samples <- tibble::rownames_to_column(devseq_transcripts_all_samples, "id")
 devseq_transcripts_all_samples_pol <- tibble::rownames_to_column(devseq_transcripts_all_samples_pol, "id")
-
-devseq_transcripts_all_samples <- devseq_transcripts_all_samples[!devseq_transcripts_all_samples$id %like% "NewAT", ]
-devseq_transcripts_all_samples_pol <- devseq_transcripts_all_samples_pol[!devseq_transcripts_all_samples_pol$id %like% "NewAT", ]
-
-####################################################################################################
-####### Optional: prepare gene_id-symbol list and add symbols to DevSeq ATH expression table #######
-# This step is only required if ATH input data table does not contain a symbol column
-
-all_devseq_transcripts <- as.data.frame(devseq_transcripts_all_samples[, 1])
-names(all_devseq_transcripts) <- c("id")
-all_devseq_transcripts$gene_id <- sub("\\..*", "", all_devseq_transcripts$id)
-
-gene_names_id <- as.data.frame(gene_id_symbol_list[, 1])
-names(gene_names_id) <- c("gene_id")
-
-devseq_transcripts_wo_symbol <- anti_join(all_devseq_transcripts, gene_names_id, by = "gene_id")
-devseq_transcripts_wo_symbol$symbol <- devseq_transcripts_wo_symbol$gene_id
-
-names(gene_id_symbol_list)[names(gene_id_symbol_list) == "id"] <- "gene_id"
-devseq_transcripts_w_symbol <- merge(all_devseq_transcripts, gene_id_symbol_list, by = "gene_id")
-devseq_transcripts_w_symbol <- devseq_transcripts_w_symbol[, c("id", "gene_id", "symbol")]
-
-gene_names_all_transcripts <- rbind(devseq_transcripts_w_symbol, devseq_transcripts_wo_symbol)
-
-addGeneSymbols <- function(df) {
-		    df <- merge(df, gene_names_all_transcripts, by = "id")
-		    df = df %>% select(id, gene_id, symbol, everything())
-		    df <- df[order(df$id), ]
-		}
-
-devseq_transcripts_all_samples <- addGeneSymbols(devseq_transcripts_all_samples)
-
-####################################################################################################
 
 
 
@@ -62,7 +23,7 @@ devseq_transcripts_all_samples <- addGeneSymbols(devseq_transcripts_all_samples)
 
 
 devseq_transcripts_all_samples_pol <- devseq_transcripts_all_samples_pol[c("id", 
-	"flowers_mature_pollen_28d_.1.", "flowers_mature_pollen_28d_.2.","flowers_mature_pollen_28d_.3.")]
+	"flowers_mature_pollen_7w_.1.", "flowers_mature_pollen_7w_.2.","flowers_mature_pollen_7w_.3.")]
 
 devseq_transcripts_all_samples <- merge(devseq_transcripts_all_samples, devseq_transcripts_all_samples_pol, by = "id")
 
@@ -78,141 +39,47 @@ names(devseq_transcripts_all_samples)[names(devseq_transcripts_all_samples) == '
 # Set final order of samples
 devseq_transcripts_all_samples = devseq_transcripts_all_samples %>% select(
 			transcript_id, 
-			gene_id,
-			symbol, 
-			root_root_tip_5d_.1., 
-			root_root_tip_5d_.2., 
-			root_root_tip_5d_.3., 
-			root_maturation_zone_5d_.1., 
-			root_maturation_zone_5d_.2., 
-			root_maturation_zone_5d_.3., 
-			root_whole_root_5d_.1., 
-			root_whole_root_5d_.2., 
-			root_whole_root_5d_.3., 
-			root_whole_root_7d_.1., 
-			root_whole_root_7d_.2., 
-			root_whole_root_7d_.3., 
-			root_whole_root_14d_.1., 
-			root_whole_root_14d_.2., 
-			root_whole_root_14d_.3.,
-			root_whole_root_21d_.1., 
-			root_whole_root_21d_.2., 
-			root_whole_root_21d_.3., 
-			hypocotyl_10d_.1., 
-			hypocotyl_10d_.2., 
-			hypocotyl_10d_.3., 
-			third_internode_24d_.1., 
-			third_internode_24d_.2., 
-			third_internode_24d_.3., 
-			second_internode_24d_.1., 
-			second_internode_24d_.2., 
-			second_internode_24d_.3., 
-			first_internode_28d._.1., 
-			first_internode_28d._.2., 
-			first_internode_28d._.3., 
-			cotyledons_7d_.1., 
-			cotyledons_7d_.2., 
-			cotyledons_7d_.3.,  
-			leaf_1.2_7d_.1., 
-			leaf_1.2_7d_.2., 
-			leaf_1.2_7d_.3., 
-			leaf_1.2_10d_.1., 
-			leaf_1.2_10d_.2., 
-			leaf_1.2_10d_.3., 
-			leaf_1.2_petiole_10d_.1., 
-			leaf_1.2_petiole_10d_.2., 
-			leaf_1.2_petiole_10d_.3., 
-			leaf_1.2_leaf_tip_10d_.1., 
-			leaf_1.2_leaf_tip_10d_.2., 
-			leaf_1.2_leaf_tip_10d_.3., 
-			leaf_5.6_17d_.1., 
-			leaf_5.6_17d_.2., 
-			leaf_5.6_17d_.3., 
-			leaf_9.10_27d_.1., 
-			leaf_9.10_27d_.2., 
-			leaf_9.10_27d_.3., 
-			leaf_senescing_35d_.1., 
-			leaf_senescing_35d_.2., 
-			leaf_senescing_35d_.3., 
-			cauline_leaves_24d_.1., 
-			cauline_leaves_24d_.2., 
-			cauline_leaves_24d_.3., 
-			apex_vegetative_7d_.1., 
-			apex_vegetative_7d_.2., 
-			apex_vegetative_7d_.3., 
-			apex_vegetative_10d_.1., 
-			apex_vegetative_10d_.2., 
-			apex_vegetative_10d_.3., 
-			apex_vegetative_14d_.1., 
-			apex_vegetative_14d_.2., 
-			apex_vegetative_14d_.3., 
-			apex_inflorescence_21d_.1., 
-			apex_inflorescence_21d_.2., 
-			apex_inflorescence_21d_.3., 
-			apex_inflorescence_28d_.1., 
-			apex_inflorescence_28d_.2., 
-			apex_inflorescence_28d_.3., 
-			apex_inflorescence_clv1_21d._.1., 
-			apex_inflorescence_clv1_21d._.2., 
-			apex_inflorescence_clv1_21d._.3.,  
-			flower_stg9_21d._.1., 
-			flower_stg9_21d._.2., 
-			flower_stg9_21d._.3., 
-			flower_stg10_11_21d._.1., 
-			flower_stg10_11_21d._.2., 
-			flower_stg10_11_21d._.3., 
-			flower_stg12_21d._.1., 
-			flower_stg12_21d._.2., 
-			flower_stg12_21d._.3., 
-			flower_stg15_21d._.1., 
-			flower_stg15_21d._.2., 
-			flower_stg15_21d._.3., 
-			flower_stg12_sepals_21d._.1., 
-			flower_stg12_sepals_21d._.2., 
-			flower_stg12_sepals_21d._.3., 
-			flower_stg15_sepals_21d._.1., 
-			flower_stg15_sepals_21d._.2., 
-			flower_stg15_sepals_21d._.3., 
-			flower_stg12_petals_21d._.1., 
-			flower_stg12_petals_21d._.2., 
-			flower_stg12_petals_21d._.3., 
-			flower_stg15_petals_21d._.1., 
-			flower_stg15_petals_21d._.2., 
-			flower_stg15_petals_21d._.3., 
-			flower_stg12_stamens_21d._.1., 
-			flower_stg12_stamens_21d._.2., 
-			flower_stg12_stamens_21d._.3., 
-			flower_stg15_stamens_21d._.1., 
-			flower_stg15_stamens_21d._.2., 
-			flower_stg15_stamens_21d._.3., 
-			flowers_mature_pollen_28d_.1., 
-			flowers_mature_pollen_28d_.2., 
-			flowers_mature_pollen_28d_.3., 
-			flower_early_stg12_carpels_21d._.1., 
-			flower_early_stg12_carpels_21d._.2., 
-			flower_early_stg12_carpels_21d._.3., 
-			flower_late_stg12_carpels_21d._.1., 
-			flower_late_stg12_carpels_21d._.2., 
-			flower_late_stg12_carpels_21d._.3., 
-			flower_stg15_carpels_21d._.1., 
-			flower_stg15_carpels_21d._.2., 
-			flower_stg15_carpels_21d._.3., 
-			fruit_stg16_siliques_28d._.1., 
-			fruit_stg16_siliques_28d._.2., 
-			fruit_stg16_siliques_28d._.3., 
-			fruit_stg17a_siliques_28d._.1., 
-			fruit_stg17a_siliques_28d._.2., 
-			fruit_stg17a_siliques_28d._.3., 
-			fruit_stg16_seeds_28d._.1., 
-			fruit_stg16_seeds_28d._.2., 
-			fruit_stg16_seeds_28d._.3., 
-			fruit_stg17a_seeds_28d._.1., 
-			fruit_stg17a_seeds_28d._.2., 
-			fruit_stg17a_seeds_28d._.3., 
-			fruit_stg18_seeds_28d._.1., 
-			fruit_stg18_seeds_28d._.2., 
-			fruit_stg18_seeds_28d._.3.
+			root_whole_root_4d_.1.:flower_stg8_7w_.3.,
+			flower_stg8_carpels_7w_.1.,
+			flower_stg8_carpels_7w_.2.,
+			flower_stg8_carpels_7w_.3.,
+			flower_stg8_stamens_7w_.1.,
+			flower_stg8_stamens_7w_.2.,
+			flower_stg8_stamens_7w_.3.,
+			flowers_mature_pollen_7w_.1.,
+			flowers_mature_pollen_7w_.2.,
+			flowers_mature_pollen_7w_.3.
 			)
+
+
+# Create string of colnames for merged replicates
+# Adjust columns that need to be excluded to table format of devseq input raw data!
+replicate_names <- unique(gsub('.{4}$', "", names(devseq_genes_all_samples[2:ncol(devseq_genes_all_samples)])))
+devseq_col_names <- c("transcript_id", replicate_names)
+
+
+# Merge replicates
+calculateAvgExpr <- function(df) {
+
+	# Split data frame by sample replicates into a list
+	# then get rowMeans for each subset, simplify output and bind to first two columns
+	
+	averaged_replicates <- data.frame(df[1],
+
+		sapply(split.default(df[2:ncol(df)], 
+			rep(seq_along(df), 
+			each = 3, 
+			length.out=ncol(df)-1)
+			), rowMeans)
+		)
+		
+	return(averaged_replicates)
+}
+
+
+# Apply calculateAvgExpr function and add column names
+devseq_replicate_transcripts <- calculateAvgExpr(devseq_transcripts_all_samples)
+colnames(devseq_replicate_transcripts) <- devseq_col_names
 
 
 
@@ -221,8 +88,8 @@ devseq_transcripts_all_samples = devseq_transcripts_all_samples %>% select(
 
 getRE <- function(x) { 
 
-	devseq_RE <- data.frame(x[1:3], 
-        as.data.frame(t(apply(x[,c(4:ncol(x))], 1, # transposes matrix output from apply function
+	devseq_RE <- data.frame(x[1], 
+        as.data.frame(t(apply(x[,c(2:ncol(x))], 1, # transposes matrix output from apply function
 	    
 	    normalize <- function(x) {
 		
@@ -241,40 +108,8 @@ getRE <- function(x) {
 
 # Apply getRE function
 devseq_transcripts_all_samples_RE <- getRE(devseq_transcripts_all_samples)
+devseq_replicate_transcripts_RE <- getRE(devseq_replicate_transcripts)
 
-
-# Create string of colnames for merged replicates
-# Adjust columns that need to be excluded to table format of devseq input raw data!
-replicate_names <- unique(gsub('.{4}$', "", names(devseq_transcripts_all_samples[4:ncol(devseq_transcripts_all_samples)])))
-replicate_names <- gsub("[.]", "", replicate_names)
-devseq_col_names <- c("transcript_id", "gene_id", "symbol", replicate_names)
-
-
-# Merge replicates
-calculateAvgExpr <- function(df) {
-
-	# Split data frame by sample replicates into a list
-	# then get rowMeans for each subset, simplify output and bind to first two columns
-	
-	averaged_replicates <- data.frame(df[1:3],
-
-		sapply(split.default(df[4:ncol(df)], 
-			rep(seq_along(df), 
-			each = 3, 
-			length.out=ncol(df)-3)
-			), rowMeans)
-		)
-		
-	return(averaged_replicates)
-}
-
-
-# Apply calculateAvgExpr function and add column names
-devseq_replicate_transcripts <- calculateAvgExpr(devseq_transcripts_all_samples)
-colnames(devseq_replicate_transcripts) <- devseq_col_names
-
-devseq_replicate_transcripts_RE <- calculateAvgExpr(devseq_transcripts_all_samples_RE)
-colnames(devseq_replicate_transcripts_RE) <- devseq_col_names
 
 
 # Round values to two digits (database numeric columns are formated to Decimal[2])
@@ -293,10 +128,10 @@ devseq_replicate_transcripts_RE <- round_df(devseq_replicate_transcripts_RE, 2)
 
 # Write final data tables to csv files and store them in /out_dir/output/data_tables
 if (!dir.exists(file.path(out_dir, "output", "data_tables"))) dir.create(file.path(out_dir, "output", "data_tables"), recursive = TRUE)
-write.table(devseq_transcripts_all_samples, file=file.path(out_dir, "output", "data_tables", "AT_devseq_transcripts_all_samples.csv"), sep=";", dec=".", row.names=FALSE, col.names=TRUE)
-write.table(devseq_transcripts_all_samples_RE, file=file.path(out_dir, "output", "data_tables", "AT_devseq_transcripts_all_samples_RE.csv"), sep=";", dec=".", row.names=FALSE, col.names=TRUE)
-write.table(devseq_replicate_transcripts, file=file.path(out_dir, "output", "data_tables", "AT_devseq_replicate_transcripts.csv"), sep=";", dec=".", row.names=FALSE, col.names=TRUE)
-write.table(devseq_replicate_transcripts_RE, file=file.path(out_dir, "output", "data_tables", "AT_devseq_replicate_transcripts_RE.csv"), sep=";", dec=".", row.names=FALSE, col.names=TRUE)
+write.table(devseq_transcripts_all_samples, file=file.path(out_dir, "output", "data_tables", "MT_devseq_transcripts_all_samples.csv"), sep=";", dec=".", row.names=FALSE, col.names=TRUE)
+write.table(devseq_transcripts_all_samples_RE, file=file.path(out_dir, "output", "data_tables", "MT_devseq_transcripts_all_samples_RE.csv"), sep=";", dec=".", row.names=FALSE, col.names=TRUE)
+write.table(devseq_replicate_transcripts, file=file.path(out_dir, "output", "data_tables", "MT_devseq_replicate_transcripts.csv"), sep=";", dec=".", row.names=FALSE, col.names=TRUE)
+write.table(devseq_replicate_transcripts_RE, file=file.path(out_dir, "output", "data_tables", "MT_devseq_replicate_transcripts_RE.csv"), sep=";", dec=".", row.names=FALSE, col.names=TRUE)
 
 
 
