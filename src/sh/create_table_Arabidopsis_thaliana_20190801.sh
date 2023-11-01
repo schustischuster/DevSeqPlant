fdd8099cfd638416bcc313ce21cfff22eebc13e7
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2023-11-01 05:29:15 +0000

    Update TABLE names

--- src/sh/create_table_Arabidopsis_thaliana_20190801.sh
+++ src/sh/create_table_Arabidopsis_thaliana_20190801.sh
@@ -18,7 +18,7 @@ show TABLES;
 # 2._Create new database tables
 
 # Create AT tables in devseq database
-CREATE TABLE devseq.Arabidopsis_thaliana_gene_tpm_20230625 (
+CREATE TABLE devseq.Arabidopsis_thaliana_gene_tpm_20231101 (
   gene_id VARCHAR(255),
   symbol VARCHAR(255),
   root_root_tip_5d_1 DECIMAL(10,2), 
@@ -158,7 +158,7 @@ CREATE TABLE devseq.Arabidopsis_thaliana_gene_tpm_20230625 (
 );
 
 
-CREATE TABLE devseq.Arabidopsis_thaliana_gene_tpm_RE_20230625 (
+CREATE TABLE devseq.Arabidopsis_thaliana_gene_tpm_RE_20231101 (
   gene_id VARCHAR(255),
   symbol VARCHAR(255),
   root_root_tip_5d_1 DECIMAL(10,2), 
@@ -298,7 +298,7 @@ CREATE TABLE devseq.Arabidopsis_thaliana_gene_tpm_RE_20230625 (
 );
 
 
-CREATE TABLE devseq.Arabidopsis_thaliana_transcript_tpm_20230625 (
+CREATE TABLE devseq.Arabidopsis_thaliana_transcript_tpm_20231101 (
   transcript_id VARCHAR(255),
   gene_id VARCHAR(255),
   symbol VARCHAR(255),
@@ -439,7 +439,7 @@ CREATE TABLE devseq.Arabidopsis_thaliana_transcript_tpm_20230625 (
 );
 
 
-CREATE TABLE devseq.Arabidopsis_thaliana_transcript_tpm_RE_20230625 (
+CREATE TABLE devseq.Arabidopsis_thaliana_transcript_tpm_RE_20231101 (
   transcript_id VARCHAR(255),
   gene_id VARCHAR(255),
   symbol VARCHAR(255),
